e37fa541d64fa1862fb6867aac3e78f5914cd268
Kieran <Kieranherr@gmail.com>
2020-08-10 16:03:21 -0500

    Created the member variables and instantiated them in the constructor

--- RoboVDino/Fleet.cs
+++ RoboVDino/Fleet.cs
@@ -8,5 +8,16 @@ namespace RoboVDino
 {
     class Fleet
     {
+        public Weapon weapon = new Weapon();
+        public Robot robo1;
+        public Robot robo2;
+        public Robot robo3;
+        public Fleet()
+        {
+            robo1 = new Robot("PlaceHolder", 0, 0, 0);
+            robo2 = new Robot("PlaceHolder", 0, 0, 0);
+            robo3 = new Robot("PlaceHolder", 0, 0, 0);
+        }
     }
+}
 }
