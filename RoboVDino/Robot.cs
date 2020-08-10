c235eef97c3b249381c57c66c6c2d376f54c336e
Kieran <Kieranherr@gmail.com>
2020-08-10 14:41:27 -0500

    Created member variables and constructor
    I added the member variables:
    public string roboName
    public int energy
    public int attackPower
    Weapon weapon
    
    I also created a constructor that took in those variables and assigned them to a Robot Object.

--- RoboVDino/Robot.cs
+++ RoboVDino/Robot.cs
@@ -8,5 +8,16 @@ namespace RoboVDino
 {
     class Robot
     {
+        public string roboName = "";
+        public int energy = 0;
+        public int attackPower = 0;
+        Weapon weapon;
+        public Robot(string roboName, int energy, int attackPower, Weapon weapon)
+        {
+            this.roboName = roboName;
+            this.energy = energy;
+            this.attackPower = attackPower;
+            this.weapon = weapon;
+        }
     }
 }
