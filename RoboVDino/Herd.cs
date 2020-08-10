32edb09b61188c34c7675e530e74069f1b62cac9
Kieran <Kieranherr@gmail.com>
2020-08-10 16:01:02 -0500

    Created the member variables and instantiated them in the constructor

--- RoboVDino/Herd.cs
+++ RoboVDino/Herd.cs
@@ -8,5 +8,15 @@ namespace RoboVDino
 {
     class Herd
     {
+        public Weapon weapon = new Weapon();
+        public Dinosaur dino1;
+        public Dinosaur dino2;
+        public Dinosaur dino3;
+        public Herd()
+        {
+            dino1 = new Dinosaur("PlaceHolder", 0, 0, 0);
+            dino2 = new Dinosaur("PlaceHolder", 0, 0, 0);
+            dino3 = new Dinosaur("PlaceHolder", 0, 0, 0);
+        }
     }
 }
