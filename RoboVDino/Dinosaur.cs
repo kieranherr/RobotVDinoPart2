5aeeb8408756e77159b94633394148830ea9cf7b
Kieran <Kieranherr@gmail.com>
2020-08-10 14:36:08 -0500

    Created member variables and constructor
    Added the member variables:
    public string dinoName
    public int dinoHealth
    public int energy 
    public int attackPower
    
    And added the constructor to take in those variables and assign them to a dinosaur object.

--- RoboVDino/Dinosaur.cs
+++ RoboVDino/Dinosaur.cs
@@ -8,5 +8,18 @@ namespace RoboVDino
 {
     class Dinosaur
     {
+        public string dinoName = "";
+        public int dinoHealth = 0;
+        public int energy = 0;
+        public int attackPower = 0;
+
+        public Dinosaur(string dinoName, int dinoHealth, int energy, int attackPower)
+        {
+            this.dinoName = dinoName;
+            this.dinoHealth = dinoHealth;
+            this.energy = energy;
+            this.attackPower = attackPower;
+        }
+
     }
 }
