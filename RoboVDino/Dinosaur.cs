ebd3c6a0919b20c86d07beafa792b7dcf939d0da
Kieran <Kieranherr@gmail.com>
2020-08-11 14:51:28 -0500

    Changed it from taking in an attackpower int it takes in a weapon object

--- RoboVDino/Dinosaur.cs
+++ RoboVDino/Dinosaur.cs
@@ -11,18 +11,18 @@ namespace RoboVDino
         public string dinoName = "";
         public int dinoHealth = 0;
         public int energy = 0;
-        public int attackPower = 0;
+        public Weapon weapon;
 
-        public Dinosaur(string dinoName, int dinoHealth, int energy, int attackPower)
+        public Dinosaur(string dinoName, int dinoHealth, int energy, Weapon weapon)
         {
             this.dinoName = dinoName;
             this.dinoHealth = dinoHealth;
             this.energy = energy;
-            this.attackPower = attackPower;
+            this.weapon = weapon;
         }
         public void Attack(Robot robot)
         {
-            robot.roboHealth -= attackPower;
+            robot.roboHealth -= weapon.ChooseAttackDino();
             energy--;
             Console.WriteLine("The health of "+robot.roboName+" is "+robot.roboHealth+" HP.");
             Console.ReadLine();
