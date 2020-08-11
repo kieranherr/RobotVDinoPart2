14c584d6c36b2d7e4ef086670436f64dba4d480b
Kieran <Kieranherr@gmail.com>
2020-08-11 10:22:42 -0500

    Finished the Weapon class. Added the ability to choose between five different attacks

--- RoboVDino/Fleet.cs
+++ RoboVDino/Fleet.cs
@@ -8,5 +8,21 @@ namespace RoboVDino
 {
     class Fleet
     {
+<<<<<<< Updated upstream
     }
 }
+=======
+        public Weapon weapon = new Weapon();
+        public Robot robo1;
+        public Robot robo2;
+        public Robot robo3;
+        public Fleet()
+        {
+            robo1 = new Robot("PlaceHolder", 0, 0, weapon);
+            robo2 = new Robot("PlaceHolder", 0, 0, weapon);
+            robo3 = new Robot("PlaceHolder", 0, 0, weapon);
+        }
+    }
+}
+
+>>>>>>> Stashed changes
