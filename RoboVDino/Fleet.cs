99cd4eb2d8b6b0d9cc46533f08325ce0feb9e9e5
Kieran <Kieranherr@gmail.com>
2020-08-11 10:40:25 -0500

    Added RoboHealth method that gives you the ability to return that robots health value.

--- RoboVDino/Fleet.cs
+++ RoboVDino/Fleet.cs
@@ -8,17 +8,20 @@ namespace RoboVDino
 {
     class Fleet
     {
+
         public Weapon weapon = new Weapon();
         public Robot robo1;
         public Robot robo2;
         public Robot robo3;
         public Fleet()
         {
-
             robo1 = new Robot("PlaceHolder", 0, 0, weapon);
             robo2 = new Robot("PlaceHolder", 0, 0, weapon);
             robo3 = new Robot("PlaceHolder", 0, 0, weapon);
         }
+        public int RobotHealth()
+        {
+            return robo1.PassRoboHealth();
+        }
     }
 }
-
