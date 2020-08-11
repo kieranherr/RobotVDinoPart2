791926e593352e5bb99812e8591dae7c3708fde9
Kieran <Kieranherr@gmail.com>
2020-08-11 10:27:21 -0500

    Fixed the fleet merge conflict

--- RoboVDino/Fleet.cs
+++ RoboVDino/Fleet.cs
@@ -7,26 +7,14 @@ using System.Threading.Tasks;
 namespace RoboVDino
 {
     class Fleet
-<<<<<<< Updated upstream
     {
-=======
-    { 
->>>>>>> Stashed changes
         public Weapon weapon = new Weapon();
         public Robot robo1;
         public Robot robo2;
         public Robot robo3;
         public Fleet()
         {
-<<<<<<< Updated upstream
-            robo1 = new Robot("PlaceHolder", 0, 0, 0);
-            robo2 = new Robot("PlaceHolder", 0, 0, 0);
-            robo3 = new Robot("PlaceHolder", 0, 0, 0);
-        }
-    }
-}
-}
-=======
+
             robo1 = new Robot("PlaceHolder", 0, 0, weapon);
             robo2 = new Robot("PlaceHolder", 0, 0, weapon);
             robo3 = new Robot("PlaceHolder", 0, 0, weapon);
@@ -34,5 +22,3 @@ namespace RoboVDino
     }
 }
 
-
->>>>>>> Stashed changes
