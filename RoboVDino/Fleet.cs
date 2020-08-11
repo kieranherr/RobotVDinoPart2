800598f068b5662133b8c8a984d3468370e6923e
Kieran <Kieranherr@gmail.com>
2020-08-11 10:24:46 -0500

    Fixed the Fleet class. Had ints instead of Weapon objects being passed in

--- RoboVDino/Fleet.cs
+++ RoboVDino/Fleet.cs
@@ -7,13 +7,18 @@ using System.Threading.Tasks;
 namespace RoboVDino
 {
     class Fleet
+<<<<<<< Updated upstream
     {
+=======
+    { 
+>>>>>>> Stashed changes
         public Weapon weapon = new Weapon();
         public Robot robo1;
         public Robot robo2;
         public Robot robo3;
         public Fleet()
         {
+<<<<<<< Updated upstream
             robo1 = new Robot("PlaceHolder", 0, 0, 0);
             robo2 = new Robot("PlaceHolder", 0, 0, 0);
             robo3 = new Robot("PlaceHolder", 0, 0, 0);
@@ -21,3 +26,13 @@ namespace RoboVDino
     }
 }
 }
+=======
+            robo1 = new Robot("PlaceHolder", 0, 0, weapon);
+            robo2 = new Robot("PlaceHolder", 0, 0, weapon);
+            robo3 = new Robot("PlaceHolder", 0, 0, weapon);
+        }
+    }
+}
+
+
+>>>>>>> Stashed changes
